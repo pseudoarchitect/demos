06228a647570d5d24917d12d266532060ae2b5d4
pseudoarchitect <pseudo.architect@gmail.com>
2012-09-22 22:19:20 +0530

    added new demo apache-cxf-contract-first

---
+++ apache-cxf-contract-first/src/main/java/com/mycompany/contractfirst/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.contractfirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
